cc8853fb16dc6d0957d8cfdfd8e3c531048cb428
Critian <cristian.perez@civica-soft.comn>
2022-05-13 18:10:24 +0200

    api

--- front/src/app/Guards/my-can-active.spec.ts
+++
@@ -1,7 +0,0 @@
-import { JwtHelperService } from '@auth0/angular-jwt';
-import { MyCanActive } from './my-can-active';
-
-describe('MyCanActive', () => {
-  it('should create an instance', () => {
-  });
-});
--- front/src/app/safe.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Safe } from './shared/safe';
-
-describe('Safe', () => {
-  it('should create an instance', () => {
-    expect(new Safe()).toBeTruthy();
-  });
-});
